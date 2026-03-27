029d8d5e4f3462fe95e516ff70d4f3ec00b37ac3
CountDookuL0rd <leon790@mail.ru>
2026-03-27 20:48:22 +0300

    ARTEMPLESHAKOV-14 [F] КПП Подключить слушатели событий к фильтрам: Поправил ошибки в выпадающем списке, а так же добавил загрузку при обновлении данных.

--- scripts/script.js
+++ scripts/script.js
@@ -5,23 +5,67 @@ const checkbox = document.querySelector('.filters__precipitation-checkbox')
 const input = document.querySelector('.filters__temperature-input')
 const options = document.querySelectorAll('.custom-select__option')
 const buttonText = document.querySelector('.custom-select__text')
+const result = document.querySelector('.result')
+const loading = document.querySelector('.loading-result')
+let preloaderTimer = null
+
+let temperature = input.value
+let precipitation = checkbox.checked
+let humidity = buttonText.textContent
+
+function preload() {
+    if (preloaderTimer) {
+        clearTimeout(preloaderTimer)
+    }
+
+    result.classList.add('hidden')
+    loading.classList.remove('hidden')
+
+    preloaderTimer = setTimeout(() => {
+        result.classList.remove('hidden')
+        loading.classList.add('hidden')
+        preloaderTimer = null
+    }, 1000)
+}
 
 function showOrHideCustomSelectorList() {
     customSelectList.classList.toggle('custom-select__options-hide')
     customSelectButton.classList.toggle('custom-select__trigger-border-hide')
     arrow.classList.toggle('custom-select__arrow-spin')
 }
-function checkBox() {}
+function checkBox() {
+    precipitation = checkbox.checked
+    preload()
+}
 
-function inPut() {}
+function inPut() {
+    temperature = input.value
+    preload()
+}
+function closeCustomSelect() {
+    customSelectList.classList.add('custom-select__options-hide')
+    customSelectButton.classList.remove('custom-select__trigger-border-hide')
+    arrow.classList.remove('custom-select__arrow-spin')
+}
+
+document.addEventListener('click', function (event) {
+    if (
+        !customSelectButton.contains(event.target) &&
+        !customSelectList.contains(event.target)
+    ) {
+        if (!customSelectList.classList.contains('custom-select__options-hide')) {
+            closeCustomSelect()
+        }
+    }
+})
 customSelectButton.addEventListener('click', showOrHideCustomSelectorList)
 checkbox.addEventListener('change', checkBox)
 input.addEventListener('input', inPut)
 options.forEach((option) => {
     option.addEventListener('click', function () {
-        options.forEach((opt) => opt.classList.remove('custom-select__option-selected'))
-        this.classList.add('custom-select__option-selected')
         buttonText.textContent = this.textContent
+        humidity = buttonText.textContent
         showOrHideCustomSelectorList()
+        preload()
     })
 })
